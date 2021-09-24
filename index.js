1867a9c74262295715922ff278810320fa26f2d6
muhamad-fadla <yuratamma@gmail.com>
2021-09-24 17:23:53 +0700

    update: add statis files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
+const path = require('path')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.send('Home Page Route'));
 
