482685bf657bcf2810d6af8417333780a9d9c934
muhamad-fadla <yuratamma@gmail.com>
2021-09-26 15:42:05 +0700

    update: index.js

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.get('/about', (req, res) => res.send('About Page Route'));
-
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
