48a16c762443b804285992207295a8d5c71f74c2
muhamad-fadla <yuratamma@gmail.com>
2021-09-26 13:46:39 +0700

    update: upgrade UI

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/', (req, res) => {
+
+	res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
 app.get('/about', (req, res) => res.send('About Page Route'));
 
