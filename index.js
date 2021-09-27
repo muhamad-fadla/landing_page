25be6dd1ad51a5da0c4eb3d71e008cd971125e20
muhamad-fadla <yuratamma@gmail.com>
2021-09-27 08:21:29 +0700

    update: optimaze content

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const expressSitemapXml = require('express-sitemap-xml')
 const dnsPrefetchControl = require('dns-prefetch-control')
 
-
 const app = express();
 const path = require('path')
 
@@ -11,7 +10,8 @@ function getUrls(){
 		{
   			url: '/',
 	  		lastMod: new Date('2021-09-01').toISOString(), // optional (specify `true` for today's date)
-	  		changeFreq: 'weekly' // optional
+	  		changeFreq: 'weekly', // optional
+	  		// priority: 1,
 		}
 	];
 }
@@ -21,9 +21,12 @@ app.use(dnsPrefetchControl({ allow: true }))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+var IS_PRUDUCTION = true;
+
 app.get('/', (req, res) => {
 
-	res.sendFile(path.join(__dirname, 'views/index.min.html'));
+	res.sendFile(path.join(__dirname, `views/index${IS_PRUDUCTION ? '.min' : ''}.html`));
 });
 
 const port = process.env.PORT || 3000;
