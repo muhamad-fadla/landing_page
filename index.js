83a0482cf59775808228a2ee2f09d9c6365e3cec
muhamad-fadla <yuratamma@gmail.com>
2021-09-27 07:28:08 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const expressSitemapXml = require('express-sitemap-xml')
+const dnsPrefetchControl = require('dns-prefetch-control')
+
 
 const app = express();
 const path = require('path')
+
+function getUrls(){
+	return [
+		{
+  			url: '/',
+	  		lastMod: new Date('2021-09-01').toISOString(), // optional (specify `true` for today's date)
+	  		changeFreq: 'weekly' // optional
+		}
+	];
+}
+
+app.use(expressSitemapXml(getUrls, 'https://my-topup.store'))
+app.use(dnsPrefetchControl({ allow: true }))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
 
-	res.sendFile(path.join(__dirname, 'views/index.html'));
+	res.sendFile(path.join(__dirname, 'views/index.min.html'));
 });
 
 const port = process.env.PORT || 3000;
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -154,50 +154,6 @@
   });
 
   /**
-   * Gallery Slider
-   */
-  new Swiper('.gallery-slider', {
-    speed: 400,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 30
-      },
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      992: {
-        slidesPerView: 5,
-        spaceBetween: 30
-      },
-      1200: {
-        slidesPerView: 7,
-        spaceBetween: 30
-      }
-    }
-  });
-
-  /**
-   * Initiate gallery lightbox 
-   */
-  const galleryLightbox = GLightbox({
-    selector: '.gallery-lightbox'
-  });
-
-  /**
    * Testimonials slider
    */
   new Swiper('.testimonials-slider', {
